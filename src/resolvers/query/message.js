d68fb695a7075115cb74f7232549b64932961a45
maxjeffwell <maxjeffwell@gmail.com>
2018-11-15 18:42:12 -0500

    finished message query

--- src/resolvers/query/message.js
+++ src/resolvers/query/message.js
@@ -3,7 +3,13 @@ const User = require('../../models/user')
 
 
 module.exports = async(root, args, context) => {
-
+  const decodedToken = context.isAuthorized();
+  const isUser = await User.findById(decodedToken.user._id);
+  if(isUser) {
+    return await Message.find();
+  } else {
+    throw('Unauthorized');
+  }
 }
 
 
