26849e1958b6ba88c8f87dd3af6f3bf5d6a9b676
Jeff Maxwell <maxjeffwell@gmail.com>
2018-11-30 19:59:34 -0500

    username still wip

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,21 @@
 const mongoose  = require('mongoose');
-const Message = require('./message');
+// const Message = require('./message');
 
 const UserSchema = new mongoose.Schema({
-  // id: mongoose.Schema.Types.ObjectId,
+  id: mongoose.Schema.Types.ObjectId,
   username: {
     type: String,
     unique: true,
     required: true
   },
-
-  email: {
+	email: {
     type: String,
     required: true
   },
-
-  password: {
+	password: {
     type: String,
     required: true
-  },
-  messages: [{
-    // type: mongoose.Schema.Types.ObjectId,
-      type: 'String',
-      ref: Message
-  }]
-
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- src/resolvers/mutation/createChatRoom.js
+++
@@ -1,6 +0,0 @@
-// const ChatRoom = require('../../models/chatRoom');
-//
-// module.exports = async(_, { input: { name, topic, messages  }}, { session }) => {
-//     const chatRoom  = await ChatRoom.create({ ...messages }).save();
-//
-// };
