f38527c81e3de831f94823efb04c29c95c83ebc8
Jeff Maxwell <maxjeffwell@gmail.com>
2018-12-01 22:20:52 -0500

    create message mutation and subscription working on postgres db

--- src/resolvers/mutation/createMessage.js
+++ src/resolvers/mutation/createMessage.js
@@ -1,7 +1,38 @@
 const { Message } = require('../../models/message');
+const { User } = require('../../models/user');
 
-module.exports = async(_, { message }, { pubsub }) => {
-  const dbMessage = await Message.create({ ...message });
-  pubsub.publish('PUBSUB_NEW_MESSAGE', { newMessage: dbMessage });
-  return true;
+// module.exports = async(_, { message }, { pubsub }) => {
+//   const dbMessage = await Message.create({ ...message });
+//   pubsub.publish('PUBSUB_NEW_MESSAGE', { newMessage: dbMessage });
+//   return true;
+// };
+
+module.exports = async(parent, args, { pubsub }) => {
+  try {
+    const dbMessage = await Message.create({
+      ... args,
+      user_id: User.id
+    });
+
+    const asyncFunc = async () => {
+      const currentUser = await User.findOne({
+        where: {
+          id: User.id,
+        },
+      });
+
+      pubsub.publish('PUBSUB_NEW_MESSAGE', {
+          newMessage: dbMessage,
+          user: currentUser
+      });
+    };
+
+    asyncFunc();
+
+    return true;
+  }
+    catch(err) {
+    console.log(err);
+    return false;
+  }
 };
