e4555010f02de3f77d2b8b7f76b7b0bd1bbd468b
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-01 18:34:09 -0500

    create message wip

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -75,6 +75,9 @@ export default {
 		user: async (message, args, { loaders }) => {
 			return await loaders.user.load(message.userId);
 		},
+		// room: async (message, args, { loaders }) => {
+		// 	return await loaders.room.load(message.roomId);
+		// }
 	},
 
 	Subscription: {
