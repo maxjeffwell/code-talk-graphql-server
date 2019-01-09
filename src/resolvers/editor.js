e41c0de0b88d9d6f4ae5fdf368dc14bdd3506fac
maxjeffwell <maxjeffwell@gmail.com>
2019-01-08 22:18:02 -0500

    fix query

--- src/resolvers/editor.js
+++ src/resolvers/editor.js
@@ -4,13 +4,12 @@ export default {
 
   Query: {
     readCode: () => ({body: ``}),
-    info: () => { `GraphQL implementation of Jeff CodeEditor`},
   },
 
   Mutation: {
-    typeCode: (root, { code }, context) => {
-      context.PostgresPubSub.publish(EVENTS.EDITOR.TYPING_CODE,
-        {typingCode: code});
+    typeCode: (root, args, context) => {
+      const { code } = args;
+      context.PostgresPubSub.publish(EVENTS.EDITOR.TYPING_CODE, {typingCode: code});
       return code;
     }
   },
--- src/schema/editor.js
+++ src/schema/editor.js
@@ -3,7 +3,6 @@ import { gql } from 'apollo-server-express';
 export default gql`
     extend type Query {
         readCode: Code!
-        info: String
     }
     
     extend type Mutation {
