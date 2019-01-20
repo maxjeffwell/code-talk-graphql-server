ad0f654587eb9b5495c4994a6c21a838aedf7925
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-20 15:35:41 -0500

    disable aliases in sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres'
+	dialect: 'postgres',
+	operatorsAliases: false,
 });
 
 const models = {
