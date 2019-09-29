0cead8dd208ad80bc0a83a132068804960fefd60
maxjeffwell <maxjeffwell@gmail.com>
2019-09-29 15:05:42 -0400

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-	extends: ["airbnb-base", "plugin:prettier/recommended"],
-	plugins: ["import", "graphql",],
+	extends: ["airbnb-base", "prettier"],
+	plugins: ["import", "graphql", "prettier"],
 	parser: "babel-eslint",
 	rules: {
 	    "graphql/template-strings": ['error', {
 	    env: "apollo",
 	    validators: 'all',
 	    }],
+		"prettier/prettier": ["error"]
 	},
 };
